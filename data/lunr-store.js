820a97f2f3f1f1d493ca451a27f2eebbdf3414f2
evanwill <evanpeterw@gmail.com>
2019-06-03 23:29:17 -0700

    implement lunr search

--- data/lunr-store.js
+++ data/lunr-store.js
@@ -2,14 +2,9 @@
 # create lunr store 
 ---
 {%- assign items = site.data[site.data.theme.metadata] -%}
+{%- assign fields = site.data.lunr-config -%}
 var store = [ 
-    {% for item in items %} 
-    { 
-        "indexId": {{ item.object-id | jsonify }},
-        "name": {{ item.interviewee | jsonify }},
-        "date": {{ item.date | jsonify }},
-        "description": {{ item.description | normalize_whitespace | jsonify }},
-        "location": {{ item.location | jsonify }}
-    }{%- unless forloop.last -%},{%- endunless -%}
-    {%- endfor -%}
+{%- for item in items -%} 
+{ "id": {{ item.object-id | jsonify }}, {% for f in fields %}{{ f.field | jsonify }}: {% if item[f.field] %}{{ item[f.field] | replace: '""','"' | jsonify }}{% else %}"none"{% endif %}{% unless forloop.last %},{% endunless %}{% endfor %} }{%- unless forloop.last -%},{%- endunless -%}
+{%- endfor -%}
 ];
